3f6050e00ddd8d5ce2a6939deb3778abe2d9ef64
davidpetrea <52080760+davidpetrea@users.noreply.github.com>
2021-12-16 00:37:46 +0200

    first commit

---
+++ components/layout.js
@@ -0,0 +1,70 @@
+import Head from 'next/head';
+import Image from 'next/image';
+import Link from 'next/link';
+
+const name = 'Hade';
+export const siteTitle = 'Basic Next.js Project';
+
+export default function Layout({ children, home }) {
+  return (
+    <div className='max-w-xl p-4 mx-auto'>
+      <Head>
+        <link rel='icon' href='/favicon.ico' />
+        <meta
+          name='description'
+          content='Learn how to build a personal website using Next.js'
+        />
+        <meta
+          property='og:image'
+          content={`https://og-image.vercel.app/${encodeURI(
+            siteTitle
+          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+        />
+        <meta name='og:title' content={siteTitle} />
+        <meta name='twitter:card' content='summary_large_image' />
+      </Head>
+      <header className='flex flex-col items-center'>
+        {home ? (
+          <>
+            <Image
+              priority
+              src='/images/fallen.jpg'
+              className="rounded-full"
+              height={144}
+              width={144}
+              alt={name}
+            />
+            <h1 className="text-3xl font-bold">{name}</h1>
+          </>
+        ) : (
+          <>
+            <Link href='/'>
+              <a>
+                <Image
+                  priority
+                  src='/images/fallen.jpg'
+                  height={108}
+                  width={108}
+                  alt={name}
+                />
+              </a>
+            </Link>
+            <h2>
+              <Link href='/'>
+                <a>{name}</a>
+              </Link>
+            </h2>
+          </>
+        )}
+      </header>
+      <main>{children}</main>
+      {!home && (
+        <div>
+          <Link href='/'>
+            <a>← Back to home</a>
+          </Link>
+        </div>
+      )}
+    </div>
+  );
+}
